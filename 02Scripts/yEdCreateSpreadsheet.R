c8329f68d51e011d82c470ece5f9cce2b2da18d9
gresch <gresch@mailbox.org>
2015-09-07 21:06:16 +0200

    Added constants and parameter to function

--- 02Scripts/yEdCreateSpreadsheet.R
+++ 02Scripts/yEdCreateSpreadsheet.R
@@ -1,20 +1,35 @@
+###########################
+# Init
+###########################
+
+# Constants
+c_JAVA_HOME         <- "C:\\Program Files\\Java\\jre1.8.0_45" # Java is needed for the xlsx package
+c_path_to_graphml   <- "./01Data/"                            # Directory to the 'graphML' files, Recursion is available
+c_path_to_output    <- "./03Output/"                          # Directory for the output files
+c_name_of_output    <- "Process Artifact Overview"            # Name for the output files
+
 # Load functions
 source("./02Scripts/yEdGraphpml.R")
 
-# Parse XML
-result <- readGraphmlFiles()
 
-#select(distinct(result, filename), filename)
+
+###########################
+# Retrieve BPMN elements 
+# from graphML files
+###########################
+
+# Parse GraphML
+result <- readGraphmlFiles(c_path_to_graphml)
+
+
+
+###########################
+# Store XLSx and CSV
+###########################
 
 # Create date for filename
 date <- substr(gsub("-", "", Sys.Date()), 3, 10)
-
 # Store XLSx
-write.xlsx(result, paste("./03Output/", date, "_Process Artifact Overview.xlsx", sep = ""))
+write.xlsx(result, paste(c_path_to_output, date, "_", c_name_of_output, ".xlsx", sep = ""), row.names = FALSE)
 # Store CVS
-write.csv(result, paste("./03Output/", date, "_Process Artifact Overview.csv")) 
-
-# Create barplots showing the complexity of processes
-
-
-
+write.csv(result, paste(c_path_to_output, date, "_" , c_name_of_output, ".csv", sep = ""), row.names = FALSE) 
--- 02Scripts/yEdGraphpml.R
+++ 02Scripts/yEdGraphpml.R
@@ -1,5 +1,9 @@
+###########################
+# Init
+###########################
+
 # Set the correct path to Java
-Sys.setenv(JAVA_HOME="C:\\Program Files\\Java\\jre1.8.0_45")
+Sys.setenv(JAVA_HOME=c_JAVA_HOME)
 
 # load libraries
 library(XML)
@@ -8,18 +12,18 @@ library(xlsx)
 library(dplyr)
 
 
-##################################
-## Read files
-##################################
+###########################
+# function to parse
+# GraphML files
+###########################
 
-readGraphmlFiles <- function() {
-  path = "./01Data/"
+readGraphmlFiles <- function(path_to_graphml) {
   out.file <- ""
-  file.names <- dir(path, pattern =".graphml", recursive=T)
+  file.names <- dir(path_to_graphml, pattern =".graphml", recursive=T)
     
   for(i in 1:length(file.names)){
     # Get information from the filename
-    data <- xmlParse(paste(path, file.names[i], sep=""))
+    data <- xmlParse(paste(path_to_graphml, file.names[i], sep=""))
     # Parse xml and build data frame
     file <- data.frame(
       filename=file.names[i],
@@ -29,9 +33,6 @@ readGraphmlFiles <- function() {
     
     out.file <- rbind(out.file, file)  
   }
-  
-  # set type to class factor
-  #out.file$type <- as.factor(out.file$type)
   
   # remove NAs 
   out.file <- out.file[is.na(out.file$filename)==0,]
